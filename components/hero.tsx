4443b103b99d832bffe92293bbf6dbd099b79d47
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-06-24 05:17:18 -0400

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <section className="mx-auto flex max-w-3xl flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center text-balance leading-tight lg:leading-[1.1]">
-        Hello! I'm weebthedev
+        Hello! I&apos;m weebthedev
       </h1>
       <p className="text-lg font-light max-w-xl text-center text-foreground">
         I aim to create websites that are clean, simple, and provide an
